23fd1d0c6bbd319d816b2e5aeb924679d67dec20
LilChooch <oleg.2018.85.ru@gmail.com>
2024-09-25 00:47:17 +0500

    Смолин Олег При-202 задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,9 +6,30 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
-
-    void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+    public Point (int x, int y)
+    {
+       this.x = x;
+       this.y = y;
     }
+
+    void flip ()
+    {
+        x = -x;
+        y = -y;
+        int val = x;
+        x = y;
+        y = val;
+    }
+    double distance(Point point)
+    {
+        double x2 = Math.pow(x - point.x, 2);
+        double y2 = Math.pow(y - point.y, 2);
+        return Math.sqrt(x2+y2);
+    }
+    public String toString()
+    {
+       return String.format("(%d, %d)", x, y);
+
+    }
+
 }
